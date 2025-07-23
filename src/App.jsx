6ed75224ad6c494cfd878be93840e69125312bcd
sdipen <sdipen302@gmail.com>
2025-07-23 11:29:35 +0530

    day2 progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
-function Task({text}){
+function Task({text,date,time,id}){
+  const [isdone, setIsdone] = useState(false);
   return(
     <>
-    <div className='card'>{text}</div>
+    <div className='taskpannel'>
+    <button onClick={()=>setIsdone((prev)=>!prev)}>{isdone?'✅':'⭕'}</button>
+    <div >{text} is due til ⏰{time?time:'not set'} of {date?date:'not set'}</div>
+    <button >🗑️</button>
+    </div>
     </>
   )
 }
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [textData,setTextData] = useState('')
 
+  const [textData,setTextData] = useState('')
+  const [tasklist,setTasklist] = useState([])
+  const [date, setDate] = useState('');
+  const [tasktime, setTasktime] = useState('');
+  
   return (
     <>
-    
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div>
-        <div>
-
-          <Task text={count}></Task>
+      <div className='card'>
+        <div className='herobar'>
+          {/* <form action={(e)=>e.} > */}
+            <input type="text" onChange={(e)=>setTextData(e.target.value)} />
+            <input type="date" onChange={(e)=>setDate(e.target.value)}/>
+            <input type="time" name="time" onChange={(e)=>setTasktime(e.target.value)}/>
+            <button type='submit' onClick={()=>setTasklist((prev)=>prev = [...prev,{textData,date,tasktime}])}>add</button>
+          {/* </form> */}
         </div>
+        <div>
+          {tasklist.length ? tasklist.map((tasks,index)=>(<Task id ={index} text={tasks.textData} date={tasks.date} time={tasks.tasktime} ></Task>)):<div></div>}
+          </div>
       </div>
       
     </>
